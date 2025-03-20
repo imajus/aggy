e002c1f7d6092198bda3cbab226c48668b867943
Jeff Wentworth <jeff@curvegrid.com>
2025-03-21 07:50:23 +0900

    initial check-in

---
+++ contracts/blockchain/scripts/deploy-mb.ts
@@ -0,0 +1,24 @@
+import hre from 'hardhat';
+
+async function main() {
+  const signers = await hre.ethers.getSigners();
+  const signer = signers[0];
+
+  await hre.mbDeployer.setup();
+  const numberOfOptions = 5;
+
+  const simpleVoting = await hre.mbDeployer.deploy(signer, 'SimpleVoting', [numberOfOptions], {
+    addressLabel: 'simple_voting',
+    contractVersion: '1.0',
+    contractLabel: 'simple_voting',
+  });
+
+  console.log(`SimpleVoting with ${numberOfOptions} options deployed to ${await simpleVoting.contract.getAddress()}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
