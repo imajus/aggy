305c31effe16d7e937d2655fa478e85ac33ceae8
Jeff Wentworth <jeff@curvegrid.com>
2025-03-21 09:22:55 +0900

    upgrade compiler

--- contracts/blockchain/contracts/SimpleVoting.sol
+++ contracts/blockchain/contracts/SimpleVoting.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.29;
 
 contract SimpleVoting {
     uint256 public numChoices;
--- contracts/blockchain/hardhat.config.ts
+++ contracts/blockchain/hardhat.config.ts
@@ -43,7 +43,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.24',
+        version: '0.8.29',
         settings: {
           optimizer: {
             enabled: true,
