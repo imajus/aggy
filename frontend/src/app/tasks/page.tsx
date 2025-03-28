d8022ffb52f2bff465a310044db5f8ce2ef6e4b0
Denis <denis.perov@gmail.com>
2025-03-28 16:46:49 +0300

    Implement Task claim & submit functions

--- frontend/src/app/tasks/page.tsx
+++ frontend/src/app/tasks/page.tsx
@@ -3,5 +3,5 @@
 import { Task, TasksList } from "@/components/tasks/TasksList";
 
 export default function TasksPage() {
-  return <TasksList title="Open Tasks" filterStatus={[0, 1]} />;
+  return <TasksList title="Open Tasks" filterStatus={[0]} />;
 } 
